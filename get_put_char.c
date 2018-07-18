01ee7325e1238ff12d4931efb731f9fcd9561480
Hawkqwerty <alexeygrigorenko59@gmail.com>
2018-07-18 09:43:47 +0300

    getchar and putchar

---
+++ get_put_char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*How do getchar and putchar work? getchar() get ONE char and return its code,
+  putchar() get code of the char and return this char. This code demonstraites behaviour of this functions*/
+
+int main()
+{
+  int c;
+
+  while ((c = getchar()) != EOF){
+    printf("%c - ", c); /*In this case you can see an ASCII code of each char. 
+			  Replace "%d" to "%c" and it automaticaly translate ANSII code to the char */
+    putchar(c);
+    printf("\n");
+  }
+}
+
+ 
